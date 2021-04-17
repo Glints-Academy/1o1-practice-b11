e5e7a09491eddae0770c3244a6222b7b02eeb458
Fariz Rizky Rizaldy <farizrizkyrizaldy@gmail.com>
2021-04-18 03:07:46 +0700

    add initial data

---
+++ GuessTheNeighbour/initialData.js
@@ -0,0 +1,43 @@
+module.exports = [
+    {
+      id: 1,
+      name: 'Joko',
+      houseColor: 'Blue'
+    }, {
+      id: 2,
+      name: 'Robert',
+      houseColor: 'Red'
+    }, {
+      id: 3,
+      name: 'Johnson',
+      houseColor: 'Green'
+    }, {
+      id: 4,
+      name: 'Phill',
+      houseColor: 'Orange'
+    }, {
+      id: 5,
+      name: 'Jay',
+      houseColor: 'Purple'
+    }, {
+      id: 6,
+      name: 'Budi',
+      houseColor: 'Maroon'
+    }, {
+      id: 7,
+      name: 'Doremi',
+      houseColor: 'Navy'
+    }, {
+      id: 8,
+      name: 'Andika',
+      houseColor: 'Blue Sky'
+    }, {
+      id: 9,
+      name: 'Jundhi',
+      houseColor: 'Grey'
+    }, {
+      id: 10,
+      name: 'Dedi',
+      houseColor: 'White'
+    },
+  ]
